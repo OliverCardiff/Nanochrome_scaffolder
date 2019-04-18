9b806bbba294b6b7c4e73b0c530682cd902edaff
Oliver Rimington <o.j.rimington@gmail.com>
2019-04-18 16:06:02 +0100

    Finished!* (it has worked once)

--- nano_confirms.py
+++ nano_confirms.py
@@ -4,11 +4,17 @@ import sys
 import pandas as pd
 
 QUANTILE_THRESH = 0.97
-CONF_WEIGHT = 30
+CONF_WEIGHT = 20
 DEGREE_FILTER = 5
 QUAL = 25
 AUTO_GAP = 2000
 TRIM_LIMIT = 500
+
+class JoinData:
+    def __init__(self, wt, auto, nano):
+        self.wt = wt
+        self.auto = auto
+        self.nano = nano
 
 class Join:
     def __init__(self, other, weight):
@@ -20,20 +26,148 @@ class Join:
         self.gap = AUTO_GAP
         self.trim_amnt = 0
         self.best = False
+        
+    def Reflect(self, scnm):       
+        res = self.other.HasFixedMatch(scnm)
+        
+        if res == 'L':
+            return [res, self.other.Left[scnm]]
+        elif res == 'R':
+            return [res, self.other.Right[scnm]]
+        else:
+            return [res, None]
     
     def __lt__(self,other):
         return self.weight < other.weight
+
+class MetaScaffold:
+    def __init__(self, scaffolds, joins, gaps, trims, jdat):
+        self.Scaffolds = scaffolds
+        self.JoinTypes = joins
+        self.Gaps = gaps
+        self.Trims = trims
+        self.Jdata = jdat
+        scln = sum([sc.length for sc in self.Scaffolds])
+        
+        self.length = scln + sum(self.Gaps) - sum(self.Trims)
+        
+        for sc in self.Scaffolds:
+            sc.in_meta = True
+            
+    def Name(self, name):
+        self.Name = name
     
+    def PrintNet(self, nodes, net):
+        for sc in self.Scaffolds:
+            nodes.write(sc.name + "_L\t" + str(sc.length) + "\n")
+            nodes.write(sc.name + "_R\t" + str(sc.length) + "\n")
+            net.write(sc.name + "_L\t" + sc.name + "_R\t2000\t3\n")
+            
+        for i in range(len(self.Jdata)):
+            sc1nm = self.Scaffolds[i].name + "_R"
+            sc2nm = self.Scaffolds[i+1].name + "_L"
+            stub = sc1nm + "\t" + sc2nm + "\t" + str(self.Jdata[i].wt)
+            auto = self.Jdata[i].auto
+            nano = self.Jdata[i].nano
+            if auto:
+                net.write(stub + "\t1\n")
+            if nano:
+                net.write(stub + "\t2\n")
+            
+    
+    def PrintIt(self, handle):
+        seqs = []
+        main_seq = ""
+        for sc in self.Scaffolds:
+            seqs.append(sc.seq)
+        
+        lastrev = False
+        
+        if len(seqs) > 0:
+            handle.write(">" + self.Name + "\n")
+            for i in range(len(seqs)):
+                tr1 = tr2 = 0
+                j1 = 'F'
+                if i > 0:
+                    tr1 = self.Trims[(i*2)-1]
+                    j1 = self.JoinTypes[(i*2)-1]
+                if i < (len(seqs) - 1):
+                    tr2 = self.Trims[i*2]
+                tr2 = len(seqs[i]) - tr2
+                
+                flip = False
+                
+                if lastrev and j1 == 'F':
+                    flip = True
+                elif not lastrev and j1 == 'R':
+                    flip = True
+                
+                if flip:
+                    seqs[i] = seqs[i][::-1] 
+                
+                main_seq += seqs[i][tr1:tr2]
+                
+                if i < (len(seqs) - 1):
+                    main_seq += 'N' * self.Gaps[i]
+                    lastrev = flip
+                    
+        mln = len(main_seq)
+        inc = 0
+        while(inc < (mln - 100)):
+            subs = main_seq[inc:(inc + 100)]
+            handle.write(subs + "\n")
+            inc += 100
+        
+        rem = mln % 100
+        if rem > 0:
+            subs = main_seq[(mln - rem):mln]
+            handle.write(subs + "\n")
+
 class Scaffold:
     def __init__(self, name, length):
         self.name = name
         self.length = length
-        self.bestpath = 0
+        self.in_meta = False
+        self.in_loop = False
+        self.best_length = 0
         self.seq = ""
         
         self.Left = {}
         self.Right = {}
         self.Both = {}
+        
+    def PrintIt(self, handle):
+        mln = len(self.seq)
+        inc = 0
+        while(inc < (mln - 100)):
+            subs = self.seq[inc:(inc + 100)]
+            handle.write(subs + "\n")
+            inc += 100
+        
+        rem = mln % 100
+        if rem > 0:
+            subs = self.seq[(mln - rem):mln]
+            handle.write(subs + "\n")
+        
+    def IsEntryPoint(self):
+        if self.Left and not self.Right:
+            return 'L'
+        if self.Right and not self.Left:
+            return 'R'
+        
+        return ''
+        
+    def RemoveTakenJoins(self):
+        def stmeta(dct):
+            ks = list(dct.keys())
+            for k in ks:
+                if dct[k].other.in_meta:
+                    del dct[k]
+                    
+        if not self.in_meta:
+            stmeta(self.Left)
+            stmeta(self.Right)
+            stmeta(self.Both)
     
     def SwitchDict(self, m1, m2, othnm):
         def SendJoin(jn, oth, m):
@@ -77,17 +211,19 @@ class Scaffold:
         else:
             return 'N'
              
-    def RunAutoConf(self):
+    def RunAutoConf(self, fraglen):
         def CheckEntry(vl, ch1, ch2):
             if vl.weight > CONF_WEIGHT:
                 res = vl.other.HasFixedMatch(self.name)
                 if res == 'L':
                     vl.orientation = ch1
                     vl.auto_confs = 1
+                    vl.gap = int(fraglen/3)
                     return 1
                 if res == 'R':
                     vl.orientation = ch2
                     vl.auto_confs = 1
+                    vl.gap = int(fraglen/3)
                     return 1
             return 0
         
@@ -299,9 +435,62 @@ class Read:
         self.alignments[othname] = aln
 
 class Graph:
-    def __init__(self):
+    def __init__(self, flen):
         self.Scaffolds = {}
         self.Reads = {}
+        self.PreN50 = 0
+        self.PostN50 = 0
+        self.FragLen = flen
+        
+    def GetSeq(self, othnm):
+        if othnm in self.Scaffolds:
+            return self.Scaffolds[othnm].seq
+        
+    def RunN50(self, lns):
+        lns = sorted(lns)
+        
+        sz = sum(lns)/2
+        accu = 0
+        n50 = 0
+        oldL = 0
+        for l in lns:
+            if accu + l > sz:
+                a = (sz - accu) / l
+                b = l - oldL
+                n50 = oldL + (b * a)  
+            else:
+                accu += l
+                oldL = l
+        
+        return n50
+    
+    def FinalStats(self, metas):
+        lns = []
+        
+        for ks,vl in self.Scaffolds.items():
+            if not vl.in_meta:
+                lns.append(vl.length)
+        
+        for m in metas:
+            lns.append(m.length)
+            
+        ln = len(lns)
+        sm = sum(lns)
+            
+        return [self.RunN50(lns), ln, sm]
+    
+    def CalcN50(self, dct):
+        
+        lns = [0] * len(dct)
+        i = 0
+        for ks,vl in dct.items():
+            lns[i] = vl.length
+            i += 1
+        
+        n50 = self.RunN50(lns)
+        
+        return n50
+        
         
     def StripUnconfirmed(self):
         rem  = nrem = scs = 0
@@ -321,7 +510,7 @@ class Graph:
         allc = cnf = 0
         
         for scs in self.Scaffolds.values():
-            allc1, cnf1 = scs.RunAutoConf()
+            allc1, cnf1 = scs.RunAutoConf(self.FragLen)
             allc += allc1
             cnf += cnf1
         
@@ -366,10 +555,13 @@ class Graph:
                 self.Scaffolds[oldID] = Scaffold(oldID, len(seq))
                 glen += len(seq)
                 snum += 1
-                
+        
+        self.PreN50 = self.CalcN50(self.Scaffolds)
+        
         print("Read Genome with " + str(glen) + " bases")
-        print("Found " + str(snum) + " separate contigs\n")
-    
+        print("Found " + str(snum) + " separate contigs")
+        print("Assembly N50 is: %.2f\n" % self.PreN50)
+        
     def ReadTable(self, tnam):
         
         def GetInc(t1, t2, tst):
@@ -665,16 +857,239 @@ class Graph:
 
 class Agent:
     def __init__(self, scdict):
-        self.Scaffolds = scdict
+        self.Scaffolds = list(scdict.values())
+        self.Metas = []
+        
+    class PreMeta:
+        def __init__(self, entry):
+            self.scaffs = []
+            self.joins = []
+            self.gaps = []
+            self.trims = []
+            self.jdat = []
+            self.entry = entry
+            
+        def LoopCheck(self):
+            for sc in self.scaffs:
+                if sc.in_loop:
+                    return False
+                
+            return True
+        
+        def Shatter(self):
+            for sc in self.scaffs:
+                sc.in_loop = True
+                
+            sc1 = self.scaffs[0]
+            
+            if self.entry == 'L':
+                sc1.Left = {}
+            if self.entry == 'R':
+                sc1.Right = {}
+            
+        def AttemptClosure(self):
+            for s in self.scaffs:
+                if s.in_meta:
+                    return None
+            
+            met = MetaScaffold(self.scaffs, self.joins,
+                               self.gaps, self.trims, self.jdat)
+            
+            return met
+            
+        def __lt__(self, other):
+            ln = len(self.scaffs)
+            ln2 = len(other.scaffs)
+            
+            if ln != ln2:
+                return ln < ln2
+            else:
+                ln = sum([sc.length for sc in self.scaffs])
+                ln2 = sum([sc.length for sc in other.scaffs])
+
+                return ln < ln2
+        
+    def StripGraph(self):
+        sc2 = []
+        for sc in self.Scaffolds:
+            sc.RemoveTakenJoins()
+            if not sc.in_meta:
+                sc2.append(sc)
+                
+        self.Scaffolds = sc2
+        
+    def Traverse(self, scaff, entry):
+        def ChooseDest(dct):
+            if len(dct) > 0:
+                best = next(iter(dct.values()))
+                
+                for ks,jn in dct.items():
+                    if jn.nano_confs and not best.nano_confs:
+                        best = jn
+                    elif jn.nano_confs == best.nano_confs:
+                        if jn.auto_confs and not best.auto_confs:
+                            best = jn
+                        elif jn.weight > best.weight:
+                            best = jn
+                return best
+            else:
+                return None
+                    
+        entry_point = entry
+        
+        journey = {}
+        pm = self.PreMeta(entry_point)
+        
+        can_move = True
+        
+        while(can_move):
+            journey[scaff.name] = scaff
+            pm.scaffs.append(scaff)
+            
+            jn = None
+            if entry_point == 'L':
+                jn = ChooseDest(scaff.Right)
+            if entry_point == 'R':
+                jn = ChooseDest(scaff.Left)
+                
+            if not jn:
+                can_move = False
+            else:
+                next_entry, jn2 = jn.Reflect(scaff.name)
+                if not jn2 or jn.other.name in journey or jn.other.in_meta:
+                    can_move = False
+                else:
+                    scaff = jn.other
+                    entry_point = next_entry
+                    pm.joins.append(jn.orientation)
+                    pm.joins.append(jn2.orientation)
+                    pm.gaps.append(jn.gap)
+                    pm.trims.append(jn.trim_amnt)
+                    pm.trims.append(jn2.trim_amnt)
+                    pm.jdat.append(JoinData(jn.weight, 
+                                            jn.auto_confs, jn.nano_confs))
+                
+        return pm
+    
+    def LoopShatter(self):
+        sh_cnt = 0
+        pms = []
+        
+        for sc in self.Scaffolds:
+            if sc.Left and sc.Right:
+                pma = self.Traverse(sc, 'L')
+                pmb = self.Traverse(sc, 'R')
+                
+                if len(pma.scaffs) > 1:
+                    pms.append(pma)
+                if len(pmb.scaffs) > 1:
+                    pms.append(pmb)
+                    
+        pms = sorted(pms, reverse=True)
+        for p in pms:
+            if p.LoopCheck():
+                p.Shatter()
+                sh_cnt += 1
+                
+        return sh_cnt
                 
         
+    def RunPaths(self):
+        metas_built = 1
+        metas_kept = 1
+        total_metas = 0
+        inc = 0
+        prems = []
+        can_shatter = True
+        lsht = 0
+        
+        tscs = len(self.Scaffolds)
+        inmetas = 0
+        
+        while(metas_kept > 0):
+            metas_built = 0
+            metas_kept = 0
+            prems = []
+            inc += 1
+            self.StripGraph()
+            
+            for sc in self.Scaffolds:
+                sc.in_loop = False
+                res = sc.IsEntryPoint()
+                if res:
+                    res = 'L' if res == 'R' else 'R'
+                    npm = self.Traverse(sc, res)
+                    
+                    if len(npm.scaffs) > 1:
+                        prems.append(npm)
+                        metas_built += 1
+            
+            if len(prems) > 1:
+                prems = sorted(prems, reverse=True)
+            
+            for pm in prems:
+                met = pm.AttemptClosure()
+                if met:
+                    metas_kept += 1
+                    total_metas += 1
+                    inmetas += len(pm.scaffs)
+                    met.Name("Nanochrome_scaffold_" + str(total_metas))
+                    self.Metas.append(met)
+                    
+            if metas_kept:
+                can_shatter = True
+            
+            print("Meta-scaffolding iteration: " + str(inc))
+            print("..new scaffolds created: " + str(metas_built))
+            print("..of which kept: " + str(metas_kept) + "\n")
+            
+            if can_shatter and inc > 1:
+                print("Running loop breaker")
+                shat = self.LoopShatter()
+                lsht += shat
+                if shat and not metas_kept:
+                    metas_kept = 1
+                print("..broke open " + str(shat) + " loops\n")
+                
+        print ("Grand Total of: " + str(total_metas) + " metascaffolds built")
+        print ("Unpicked " + str(lsht) + " tangles in graph")
+        print ("Of " + str(tscs) + " scaffolds in genome, " 
+               + str(inmetas) + " were used\n")
+                        
+        
+    def PrintNetwork(self, pref):
+        nodes = pref + "_nodes_final.tsv"
+        net = pref + "_net_final.tsv"
+        
+        with open(nodes, "w") as ndfile:
+            with open(net, "w") as ntfile:
+                ndfile.write("NODE\tLENGTH\n")
+                ntfile.write("SOURCE\tTARGET\tWEIGHT\tTYPE\n")
+                for m in self.Metas:
+                    m.PrintNet(ndfile, ntfile)
+        
+        print("Wrote " + nodes + " (network nodes)")
+        print("Wrote " + net + " (network edges)\n")
+                   
+    def PrintMetas(self, pref, sc_resid):
+        genm = pref + "_final_genome.fa"
+        with open(genm, "w") as genomefile:
+            for m in self.Metas:
+                m.PrintIt(genomefile)
+            
+            for ks,sc in sc_resid.items():
+                if not sc.in_meta:
+                    sc.PrintIt(genomefile)
+        print("Wrote " + genm + " successfully!\n")
+       
 def HelpMsg():
     print ("\nNanoChrome - Confirming 10x with nanopore\n")
     print ("Input:\n")
     print ("ARG1: <genome.fa> A Genome file")
     print ("ARG2: <nc_table.tsv> output from chrome_candidates.py")
-    print ("ARG3: <alns.sam> Mapped Longread library")
-    print ("ARG4: <prefix> Unique prefix for outputs\n")
+    print ("ARG3: <alns.paf> Mapped Longread library")
+    print ("ARG4: <integer> Fragment Length (x10 library)")
+    print ("ARG5: <prefix> Unique prefix for outputs\n")
     print ("Output:\n")
     print ("<prefix>_nc_scaffolded.fa: Final Scaffolded Genome")
     print ("<prefix>_network_final.tsv: A network file for visualisation")
@@ -683,9 +1098,9 @@ def HelpMsg():
 def main(argv):
 
     # make sure there are at least three arguments
-    if len(argv) >= 4:
+    if len(argv) >= 5:
         #try:
-        the_graph = Graph()
+        the_graph = Graph(int(argv[3]))
         print("Scanning the genome...\n")
         the_graph.ScanGenome(argv[0])
         print("Reading the edge graph...\n")
@@ -699,11 +1114,18 @@ def main(argv):
         the_graph.StripUnconfirmed()
         print("Agent navigating graph...\n")
         the_agent = Agent(the_graph.Scaffolds)
-        
+        the_agent.RunPaths()
         print("Fixing in optimal scaffold paths\n")
+        the_agent.PrintNetwork(argv[4])
+        print("Writing final assembly\n")
         the_graph.StoreGenome(argv[0])
-        print("Writing Final Assembly\n")
-            
+        the_agent.PrintMetas(argv[4], the_graph.Scaffolds)
+        
+        n50, cnt, sz = the_graph.FinalStats(the_agent.Metas)
+        
+        print("The new genome size: " + str(sz))
+        print("..with contig count: " + str(cnt))
+        print("..and an n50 of: %.2f\n" % n50)
             
         #except:
         #    print("Error: ",sys.exc_info()[0]," <- this happened.")
